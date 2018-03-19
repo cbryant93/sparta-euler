7dcf3d5e223564a1154e2c55d8e59b263a23ef34
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-19 17:05:24 +0000

    Project 1 update

--- project1.rb
+++ project1.rb
@@ -3,37 +3,23 @@ class Multiples
 
   def initialize
     @num_arr = []
+    @total_arr = 0
 
   end
 
   def range(s,e)
-    (s..e).each do |i|
-      num_arr << sum(i)
+    for i in s..e
+    if(divisible_by(i,3)||divisible_by(i,5))
+      num_arr << i
     end
   end
 
-  def divisible_by(num,div)
-    if (num%div).zero?
-      return true
-    else
-      return false
-    end
-  end
+def print_total
+  puts arr.sum
+end
 
-def sum(i)
-
-  if divisible_by(i,5)
-
-
- elsif divisible_by(i,3)
-
-
-  else
-   puts i
-  end
-
+def divisble_by(i,d)
+  (i%d).zero?
 end
 
 end
-
-Sum.new.range(1,1000)
