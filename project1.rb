0a4efc960571af6a257a80a7c81c8f76cabe142e
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-19 20:42:20 +0000

    fixes

--- project1.rb
+++ project1.rb
@@ -4,22 +4,23 @@ class Multiples
   def initialize
     @num_arr = []
     @total_arr = 0
+  end
 
+  def divisble_by(i,d)
+    (i%d).zero?
   end
 
   def range(s,e)
-    for i in s..e
-    if(divisible_by(i,3)||divisible_by(i,5))
+    (s..e).each do |i|
+    if(divisble_by(i,3)||divisble_by(i,5))
       num_arr << i
     end
   end
-
-def print_total
-  puts arr.sum
 end
 
-def divisble_by(i,d)
-  (i%d).zero?
+  def print_total
+    puts num_arr.sum
+  end
 end
 
-end
+Multiples.new.range(1,999)
